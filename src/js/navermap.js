bf1eac4762a7d249d68f4ef2298c2f4df6e17191
jjeongee <starstar117@naver.com>
2024-08-02 17:28:44 +0900

    설정조정

--- src/js/navermap.js
+++ src/js/navermap.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var defaultMapSetting = {
         center: new naver.maps.LatLng(37.6009, 126.8643),
-        zoom: 10
+        zoom: 15
     };
     var naverMap = new naver.maps.Map('navermap_div', defaultMapSetting);
 });
